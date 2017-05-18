e222edb45523d638126d2efdb7154d8a3b17210d
Tim <tjcutt@gmail.com>
2017-05-18 15:54:38 -0600

    addition CRUD work

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -1,34 +1,51 @@
-
 'use strict';
 
 const express = require('express');
 const knex = require('../knex');
 const router = express.Router();
 
+
 router.get("/", (req, res, next) => {
-  return knex("classifieds")
-    .select(["id", "title", "description", "price", "item_image"])
-    .then((classifieds) => {
-    res.json(classifieds)
-    })
-    .catch( err => {
-      res.send(err)
-    })
+    return knex("classifieds")
+        .select(["id", "title", "description", "price", "item_image"])
+        .then((classifieds) => {
+            res.json(classifieds)
+        })
+        .catch(err => {
+            res.send(err)
+        })
 
 });
 
 router.get('/:id', (req, res, next) => {
+    console.log(req.params.id);
     return knex('classifieds')
         .where('id', req.params.id)
         .select(["id", "title", "description", "price", "item_image"])
         .then(classifieds => {
-            res.json(classifieds);
+            res.json(classifieds[0]);
         })
-        .catch( err => {
-          res.send(err)
+        .catch(err => {
+            res.send(err)
         })
 });
 
-
+router.post('/', (req, res, next) => {
+    knex('classifieds')
+        .returning(['id', 'title', 'description', 'price', 'item_image'
+      ])
+        .insert(
+              title: req.body.title,
+              description: req.body.description,
+              price: req.body.price,
+              itemImage:
+            )
+        .then(classifieds => {
+            res.json(classifieds)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
 
 module.exports = router;
