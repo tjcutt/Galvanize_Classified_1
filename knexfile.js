913f153d62999f1e2e186684cfc903b508505cda
Tim <tjcutt@gmail.com>
2017-05-17 21:14:47 -0600

    inital commit

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,18 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_dev'
+  },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_test'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 };
