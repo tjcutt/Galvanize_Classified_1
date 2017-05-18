ddbe87526e9db5996e877eb56fcefc5fdb602aef
Tim <tjcutt@gmail.com>
2017-05-18 16:57:35 -0600

    patch and delete added. still not passing tests

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const knex = require('../knex');
 const router = express.Router();
-
+// const humps = require('humps');
 
 router.get("/", (req, res, next) => {
     return knex("classifieds")
@@ -30,22 +30,41 @@ router.get('/:id', (req, res, next) => {
         })
 });
 
-router.post('/', (req, res, next) => {
+router.post('/',(req, res, next) => {
     knex('classifieds')
-        .returning(['id', 'title', 'description', 'price', 'item_image'
-      ])
-        .insert(
-              title: req.body.title,
-              description: req.body.description,
-              price: req.body.price,
-              itemImage:
-            )
+        .insert(params(req))
+        .returning('*')
         .then(classifieds => {
-            res.json(classifieds)
+            res.json(classifieds[0])
         })
         .catch(err => {
             res.send(err)
         })
+})
+
+router.patch('/:id',(req, res, next) => {
+    knex('classifieds')
+        .update(params(req))
+        .where({
+            id: req.params.id
+        })
+        .returning('*')
+        .then(classifieds =>
+            res.json(classifieds[0]))
+        .catch(err =>
+            res.send(err))
+})
+
+router.delete('/:id', (req, res, next) => {
+    knex('classifieds')
+        .del()
+        .where({
+            id: req.params.id
+        })
+        .then(() =>
+            res.end())
+        .catch((err) =>
+            res.send(err))
 })
 
 module.exports = router;
